e6e4f9bf8da32d39a5c555545f873c03be180b79
Thor Sideburns <thorsten.sideb0ard@gmail.com>
2016-06-21 18:00:43 -0700

    multiple key patterns

--- keys.c
+++ keys.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <poll.h>
 #include <termios.h>
@@ -18,7 +19,15 @@ extern bpmrrr *b;
 extern pthread_cond_t bpm_cond;
 extern pthread_mutex_t bpm_lock;
 
-melody_loop mloop;
+//// TODO - make this instance variable - not global
+//melody_loop mloop;
+
+melody_loop* new_melody_loop()
+{
+    melody_loop* l;
+    l = (melody_loop*) calloc(1, sizeof(melody_loop));
+    return l;
+}
 
 void keys(int soundgen_num)
 {
@@ -36,13 +45,15 @@ void keys(int soundgen_num)
     int ch = 0;
     int quit = 0;
     int recording = 0;
-    int recording_started = 0;
+    //int recording_started = 0;
 
     struct pollfd fdz[1];
     int pollret;
 
     fdz[0].fd = STDIN_FILENO;
     fdz[0].events = POLLIN;
+
+    melody_loop* mloop;
 
     while (!quit) {
 
@@ -62,7 +73,7 @@ void keys(int soundgen_num)
                     case 0:
                         printf("Recording Mode OFF.\n");
                         pthread_t melody_looprrr;
-                        if ( pthread_create (&melody_looprrr, NULL, play_melody_loop, NULL)) {
+                        if ( pthread_create (&melody_looprrr, NULL, play_melody_loop, mloop)) {
                             fprintf(stderr, "Err running loop\n");
                         }
                         pthread_detach(melody_looprrr);
@@ -70,87 +81,45 @@ void keys(int soundgen_num)
                         break;
                     case 1:
                         printf("Recording Mode ON.\n");
+                        mloop = new_melody_loop();
+                        melody_event* e = make_melody_event(0, 0, b->cur_tick);
+                        add_melody_event(mloop, e);
                 }
             }
 
             double freq = chfreqlookup(ch);
             if ( freq != -1 ) {
-                if (recording && !recording_started) {
-                    recording_started = 1;
-                }
-                play_note(freq, soundgen_num, recording);
+                //if (recording && !recording_started) {
+                //    recording_started = 1;
+                //}
+                play_note(freq, soundgen_num, recording, mloop);
             }
         }
     }
     tcsetattr(0, TCSANOW, &old_info);
 }
 
-//melody_event make_melody_event(double freq, int sg_num, int tick) {
-//    melody_msg mm;
-//    mm.freq = freq;
-//    mm.sg_num = sg_num;
-//
-//    melody_event e;
-//    e.tick = tick;
-//    e.mm = mm;
-//
-//    return mm;
-//}
+melody_event* make_melody_event(double freq, int sg_num, int tick)
+{
+    melody_event* me;
+    me = (melody_event*) calloc(1, sizeof(melody_event));
+    me->tick = tick;
+
+    me->mm.freq = freq;
+    me->mm.sg_num = sg_num;
+
+    return me;
+}
     
-void add_melody_event(melody_event e)
+void add_melody_event(melody_loop* mloop, melody_event* e)
 {
     printf("ADDING melody event..\n");
-    if ( mloop.size < 10 ) {
-        mloop.melody[mloop.size] = e;
-        mloop.size++;
+    if ( mloop->size < 100 ) {
+        mloop->melody[mloop->size++] = e;
     }
 }
 
-void *play_melody_loop(void *m)
-{
-    (void) m;
-
-    printf("PLAY melody starting..\n");
-
-    int mnum = mloop.melody[0].tick;
-    printf("Starting num is %d\n", mnum);
-    melody_loop loop;
-    loop.size = 0;
-
-    for ( int i = 0; i < mloop.size; i++ ) {
-        printf("EVENT: [%d] - %f\n", mloop.melody[i].tick, mloop.melody[i].mm.freq);
-        melody_event me;
-        me.tick = mloop.melody[i].tick - mnum;
-        me.mm = mloop.melody[i].mm;
-        loop.melody[i] = me;
-        printf("LOOP size %d\n", loop.size);
-        loop.size++;
-    }
-
-    for ( int i = 0; i < loop.size; i++) {
-        printf("NEW EVENT: [%d] - %f\n", loop.melody[i].tick, loop.melody[i].mm.freq);
-    }
-
-    int final_len = loop.melody[loop.size-1].tick;
-    int diff = final_len % TICK_SIZE;
-    int diff2 = TICK_SIZE - diff;
-    int final_totes = final_len + diff2;
-
-    while (1) {
-        for ( int i = 0; i < loop.size; i++ ) {
-            if ( b->cur_tick % final_totes == loop.melody[i].tick )
-                play_note(loop.melody[i].mm.freq, loop.melody[i].mm.sg_num, 0);
-        }
-
-        //printf("TICKY! %d\n", b->cur_tick);
-        pthread_mutex_lock(&bpm_lock);
-        pthread_cond_wait(&bpm_cond,&bpm_lock);
-        pthread_mutex_unlock(&bpm_lock);
-    }
-    return NULL;
-}
-
-void play_note(double freq, int sg_num, int recording) {
+void play_note(double freq, int sg_num, int recording, melody_loop* mloop) {
 
     struct timespec ts;
     ts.tv_sec = 0;
@@ -158,15 +127,10 @@ void play_note(double freq, int sg_num, int recording) {
     double vol = 0;
 
     if ( recording ) {
-        melody_msg mm;
-        mm.freq = freq;
-        mm.sg_num = sg_num;
 
-        melody_event e;
-        e.tick = b->cur_tick;
-        e.mm = mm;
+        melody_event* e = make_melody_event(freq, sg_num, b->cur_tick);
         printf("FREQ!(%d)[%f]\n", b->cur_tick, freq);
-        add_melody_event(e);
+        add_melody_event(mloop, e);
     }
 
     mfm(mixr->sound_generators[sg_num], "car", freq);
@@ -185,4 +149,49 @@ void play_note(double freq, int sg_num, int recording) {
     }
     mixr->sound_generators[sg_num]->setvol(mixr->sound_generators[sg_num], 0.0);
 
+}
+
+void *play_melody_loop(void *m)
+{
+    melody_loop* mloop = (melody_loop*) m;
+
+    printf("PLAY melody starting..\n");
+
+    int mnum = mloop->melody[0]->tick;
+    printf("Starting num is %d\n", mnum);
+    melody_loop loop;
+    loop.size = 0;
+
+    for ( int i = 0; i < mloop->size; i++ ) {
+        printf("EVENT: [%d] - %f\n", mloop->melody[i]->tick, mloop->melody[i]->mm.freq);
+        melody_event* me = (melody_event*) calloc(1, sizeof(melody_event));
+        me->tick = mloop->melody[i]->tick - mnum;
+        me->mm = mloop->melody[i]->mm;
+        loop.melody[i] = me;
+        printf("LOOP size %d\n", loop.size);
+        loop.size++;
+    }
+
+    for ( int i = 0; i < loop.size; i++) {
+        printf("NEW EVENT: [%d] - %f\n", loop.melody[i]->tick, loop.melody[i]->mm.freq);
+    }
+
+    int final_len = loop.melody[loop.size-1]->tick;
+    int diff = final_len % TICK_SIZE;
+    int diff2 = TICK_SIZE - diff;
+    int final_totes = final_len + diff2;
+
+    while (1) {
+        for ( int i = 0; i < loop.size; i++ ) {
+            if ( b->cur_tick % final_totes == loop.melody[i]->tick )
+                play_note(loop.melody[i]->mm.freq, loop.melody[i]->mm.sg_num, 0, NULL);
+        }
+
+        //printf("TICKY! %d\n", b->cur_tick);
+        pthread_mutex_lock(&bpm_lock);
+        pthread_cond_wait(&bpm_cond,&bpm_lock);
+        pthread_mutex_unlock(&bpm_lock);
+    }
+    // TODO free all this memory!!
+    return NULL;
 }
--- keys.h
+++ keys.h
@@ -12,16 +12,18 @@ typedef struct melody_event {
 } melody_event;
 
 typedef struct melody_loop {
-    melody_event melody[10];
+    melody_event* melody[100];
     int size;
 } melody_loop;
 
 
 void keys(int soundgen_num);
-void play_note(double freq, int sg_num, int recording); // recording is a boolean
+void play_note(double freq, int sg_num, int recording, melody_loop* mloop); // recording is a boolean
 
-melody_event make_melody_event(double freq, int sg_num, int tick);
-void add_melody_event(melody_event);
+melody_loop* new_melody_loop(void);
+melody_event* make_melody_event(double freq, int sg_num, int tick);
+
+void add_melody_event(melody_loop*, melody_event*);
 void *play_melody_loop(void *m);
 
 #endif // KEYS_H_
