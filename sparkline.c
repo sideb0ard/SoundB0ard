487889a0e63fbbdfd995d806fdb5a073a4a7f06e
Thor Sideburns <thorsten.sideb0ard@gmail.com>
2017-01-07 17:17:47 -0800

    working sparklines

--- sparkline.c
+++ sparkline.c
@@ -7,20 +7,16 @@
 #include "sparkline.h"
 #include "utils.h"
 
-// const wchar_t *sparkchars =
-// L"\u2581\u2582\u2583\u2584\u2585\u2586\u2587\u2588";
+#define MAXCHARS 256 // pretty arbitrary
+
 const wchar_t *sparkchars = L"\u2581\u2582\u2583\u2585\u2586\u2587";
 
 void sparky(char *instr)
 {
-    // I'm making an assumption that input are
-    // floats in the range 0.0 - 1.0
-
-    printf("GOts %s\n", instr);
-    char str[80]; // arbitrary number here
+    char str[MAXCHARS];
     strcpy(str, instr);
 
-    double sparkvals[80] = {0};
+    double sparkvals[MAXCHARS] = {0};
     int numvals = 0;
 
     char *sep = ",; ";
@@ -30,32 +26,16 @@ void sparky(char *instr)
         sparkvals[numvals++] = atof(word);
     }
 
-    double min, max;
+    //int sparklen = numvals*2 + 1;
+    int sparklen = numvals + 1;
+    wchar_t sparkline[sparklen];
     for (int i = 0; i < numvals; i++) {
-        // printf("SVAL: %f\n", sparkvals[i]);
-        if (!min || sparkvals[i] < min)
-            min = sparkvals[i];
-        if (!max || sparkvals[i] > max)
-            max = sparkvals[i];
+        int idx = (int)floor(scaleybum(0, 1.1, 0, wcslen(sparkchars), sparkvals[i]));
+        sparkline[i] = sparkchars[idx];
+        //sparkline[i*2] = sparkchars[idx];
+        //sparkline[i*2+1] = sparkchars[idx];
     }
-    printf("Max: %.2f Min: %.2f\n", max, min);
-
-    int charset_len = wcslen(sparkchars);
-    printf("Len %d\n", charset_len);
-    double diff = max - min;
-    double skip = diff == 0 ? 1 : charset_len / diff;
-    printf("Skip %f\n", skip);
-
-    for (int i = 0; i < numvals; i++) {
-        // double v = (sparkvals[i] - min) * skip;
-        // TODO - not really a sparkline now
-        // as i'm making data assumptions
-        // rather than just showing variations,
-        // i want to
-        int idx = (int)floor(scaleybum(0, 1, 0, 5, sparkvals[i]));
-        // printf("IDX: %d\n", idx);
-        wprintf(L"%lc%lc", sparkchars[idx], sparkchars[idx]);
-        // printf("SVAL: %f\n", sparkvals[i]);
-    }
+    sparkline[sparklen] = '\0';
+    wprintf(L"FULL WURD: %ls\n", sparkline);
     printf("\n");
 }
