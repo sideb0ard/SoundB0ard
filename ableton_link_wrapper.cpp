6d9283abb9dbcbafd9a4eaf7b5972bae9ffddf2e
Thor Sideburns <thorsten.sideb0ard@gmail.com>
2018-01-15 16:15:56 -0800

    removing unused cruft from ableton wrapper

--- ableton_link_wrapper.cpp
+++ ableton_link_wrapper.cpp
@@ -15,7 +15,7 @@
 extern mixer *mixr;
 
 const auto MICROS_PER_SAMPLE = 1e6 / (double) SAMPLE_RATE;
-const auto midi_tick_len_as_percent = 1. / 960;
+const auto MIDI_TICK_FRAC_OF_BEAT = 1. / 960;
 
 struct AbletonLink
 {
@@ -25,24 +25,13 @@ struct AbletonLink
         m_host_time_filter;
 
     std::chrono::microseconds m_hosttime; // also updated every callback
-
     std::chrono::microseconds m_buffer_begin_at_output;
+
     double m_quantum;
     double m_requested_tempo;
     bool m_reset_beat_time;
-    bool m_have_started;
 
     int m_sample_time;
-    int m_sample_time_last; // TEMP
-    int m_sample_at_last_loop;
-    int m_sample_at_last_quarter;
-    int m_sample_at_last_midi_tick;
-    int m_sample_at_last_sixteenth;
-
-    double m_midi_ticks_per_ms;
-    double m_samples_per_midi_tick;
-    double m_loop_len_in_samples;
-    double m_loop_len_in_ticks;
 
     std::chrono::microseconds m_output_latency;
 
@@ -52,17 +41,7 @@ struct AbletonLink
           m_quantum{4.},
           m_requested_tempo{0},
           m_reset_beat_time{false},
-          m_have_started{false},
           m_sample_time{0},
-          m_sample_time_last{0}, // TMP
-          m_sample_at_last_loop{0},
-          m_sample_at_last_quarter{0},
-          m_sample_at_last_midi_tick{0},
-          m_sample_at_last_sixteenth{0},
-          m_midi_ticks_per_ms{PPQN / ((60.0 / bpm) * 1000)},
-          m_samples_per_midi_tick{(60.0 / bpm * SAMPLE_RATE) / PPQN},
-          m_loop_len_in_samples{(60.0 / bpm * SAMPLE_RATE) * m_quantum},
-          m_loop_len_in_ticks{PPBAR},
           m_output_latency(0)
     {
         m_link.setTempoCallback(update_bpm);
@@ -115,10 +94,9 @@ void link_update_from_main_callback(AbletonLink *l, int num_frames)
 
 inline void link_inc_midi(AbletonLink *l, mixer_timing_info *timing_info, double beat_at_time)
 {
-    timing_info->is_midi_tick = true;
-    //std::cout << "MIDI TICK! " << timing_info->midi_tick << " BEATTIME:" << beat_at_time  << std::endl;
-    timing_info->time_of_next_midi_tick = (double)((int)beat_at_time) + ((timing_info->midi_tick%PPQN) * midi_tick_len_as_percent);
+    timing_info->time_of_next_midi_tick = (double)((int)beat_at_time) + ((timing_info->midi_tick%PPQN) * MIDI_TICK_FRAC_OF_BEAT);
     timing_info->midi_tick++;
+    timing_info->is_midi_tick = true;
 }
 
 void link_update_mixer_timing_info(AbletonLink *l, mixer_timing_info *timing_info, int frame_num)
@@ -156,38 +134,6 @@ LinkData link_get_timing_data_for_display(AbletonLink *l)
     return data;
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// called every sample
-link_callback_timing_data link_get_callback_timing_data(AbletonLink *l, int sample_number)
-{
-    link_callback_timing_data data;
-    data.is_midi_tick = false;
-    data.is_start_of_loop = false;
-    data.is_start_of_quarter = false;
-    data.is_start_of_sixteenth = false;
-    data.sx_tick = 0;
-
-    const auto this_sample_time = l->m_hosttime + std::chrono::microseconds(llround(sample_number * MICROS_PER_SAMPLE));
-    const auto last_sample_time = this_sample_time - std::chrono::microseconds(llround(MICROS_PER_SAMPLE));
-
-    if (l->m_timeline.phaseAtTime(this_sample_time, l->m_quantum) < l->m_timeline.phaseAtTime(last_sample_time, l->m_quantum))
-    {
-        const auto beat = l->m_timeline.beatAtTime(this_sample_time, l->m_quantum);
-        printf("LOOPY! Beat: %f  --  %f(%d) %f(%d) // diff is %d looplen in samples is %f\n",
-                beat,
-                l->m_timeline.phaseAtTime(this_sample_time, l->m_quantum),
-                l->m_sample_time,
-                l->m_timeline.phaseAtTime(last_sample_time, l->m_quantum),
-                l->m_sample_at_last_loop,
-                l->m_sample_time - l->m_sample_at_last_loop,
-                l->m_loop_len_in_samples
-        );
-        l->m_sample_at_last_loop = l->m_sample_time;
-    }
-
-    return data;
-}
-
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void link_set_bpm(AbletonLink *l, double bpm)
@@ -220,26 +166,5 @@ double link_get_phase_at_time(AbletonLink *l, long long int sample_number, int q
 }
 
 int link_get_sample_time(AbletonLink *l) { return l->m_sample_time; }
-
-void link_inc_sample_time(AbletonLink *l) { l->m_sample_time++; }
-
-void link_set_old_sample_time(AbletonLink *l)
-{
-    l->m_sample_time_last = l->m_sample_time;
-}
-int link_get_old_sample_time(AbletonLink *l)
-{
-    return l->m_sample_time_last;
-}
-
-int link_get_samples_per_midi_tick(AbletonLink *l)
-{
-    return l->m_samples_per_midi_tick;
-}
-
-int link_get_loop_len_in_samples(AbletonLink *l)
-{
-    return l->m_loop_len_in_samples;
-}
 
 } // extern "C"
--- ableton_link_wrapper.h
+++ ableton_link_wrapper.h
@@ -15,27 +15,16 @@ typedef struct LinkData {
     double phase;
 } LinkData;
 
-typedef struct link_callback_timing_data {
-    bool is_midi_tick;
-    bool is_start_of_loop;
-    bool is_start_of_quarter;
-    bool is_start_of_sixteenth;
-    int  sx_tick; // used in sequencer to keep sixteenth ticks in sync
-} link_callback_timing_data;
-
 AbletonLink *new_ableton_link(double bpm);
 void link_update_from_main_callback(AbletonLink *l, int num_frames);
 
 void link_set_latency(AbletonLink *l, double latency);
 
 LinkData link_get_timing_data_for_display(AbletonLink *l);
-link_callback_timing_data link_get_callback_timing_data(AbletonLink *l, int sample_num);
 
 void update_bpm(double bpm);
 
 int link_get_sample_time(AbletonLink *l);
-int link_get_samples_per_midi_tick(AbletonLink *l);
-int link_get_loop_len_in_samples(AbletonLink *l);
 double link_get_bpm(AbletonLink *l);
 double link_get_beat_at_time(AbletonLink *l, long long int sample_number);
 double link_get_phase_at_time(AbletonLink *l, long long int sample_number, int quantum);
@@ -43,13 +32,8 @@ double link_get_current_quantum(AbletonLink *l);
 
 void link_update_mixer_timing_info(AbletonLink *l, mixer_timing_info *info, int frame_num);
 
-//temp
-void link_set_old_sample_time(AbletonLink *l);
-int link_get_old_sample_time(AbletonLink *l);
-
 void link_set_bpm(AbletonLink *l, double bpm);
 void link_reset_beat_time(AbletonLink *l);
-void link_inc_sample_time(AbletonLink *l);
 
 #ifdef __cplusplus
 } // end extern "C"
