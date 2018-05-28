f939517a50ebd22ea7f4c4cbbe5ee080bd493ff1
Thor Sideburns <thorsten.sideb0ard@gmail.com>
2018-05-27 18:09:35 -0700

    yolde!

--- looper.c
+++ looper.c
@@ -146,6 +146,7 @@ void looper_event_notify(void *self, unsigned int event_type)
 
         g->started = true;
         g->step_diff = 0;
+        g->reverse_mode = false;
 
         if (g->scramble_pending)
         {
@@ -205,22 +206,37 @@ void looper_event_notify(void *self, unsigned int event_type)
                         (g->stutter_idx * g->size_of_sixteenth) +
                         rel_pos_within_a_sixteenth;
                 }
-                int idx = mixr->timing_info.midi_tick % PPBAR;
-                if (g->m_seq.patterns[g->m_seq.cur_pattern][idx].event_type ==
-                    MIDI_ON)
+                else
                 {
-                    int cur_sixteenth =
-                        mixr->timing_info.sixteenth_note_tick % 16;
-                    int randy = rand() % 100;
-                    if (randy < 20)
-                        g->step_diff = 4 - cur_sixteenth;
-                    else if (randy < 50)
-                        g->step_diff = 8 - cur_sixteenth;
-                    else
+                    int idx = mixr->timing_info.midi_tick % PPBAR;
+                    if (g->m_seq.patterns[g->m_seq.cur_pattern][idx]
+                            .event_type == MIDI_ON)
+                    {
+                        int cur_sixteenth =
+                            mixr->timing_info.sixteenth_note_tick % 16;
                         g->step_diff = 0 - cur_sixteenth;
+                        looper_set_reverse_mode(g, false);
+                        g->grain_pitch = 1;
+
+                        int randy = rand() % 100;
+                        if (randy < 20)
+                            g->step_diff = 4 - cur_sixteenth;
+                        else if (randy < 50)
+                            g->step_diff = 8 - cur_sixteenth;
+                        else if (randy < 55)
+                            g->step_diff = 12 - cur_sixteenth;
+                        else if (randy < 60)
+                            looper_set_reverse_mode(g, true);
+                        //else if (randy < 63)
+                        //    g->grain_pitch = 1.2;
+                        //else if (randy < 65)
+                        //    g->grain_pitch = 1.5;
+                        //else if (randy < 67)
+                        //    g->grain_pitch = .8;
+                    }
+                    g->audio_buffer_read_idx =
+                        new_read_idx + (g->step_diff * g->size_of_sixteenth);
                 }
-                g->audio_buffer_read_idx =
-                    new_read_idx + (g->step_diff * g->size_of_sixteenth);
             }
         }
         break;
