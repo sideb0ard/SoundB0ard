97f73f298b7cdff2df99d50aba3569bfac8a212c
Thor Sideburns <thorsten.sideb0ard@gmail.com>
2019-12-23 17:45:24 -0800

    adding Env to ps cmd and started on pattern_parser tests

---
+++ tests/pattern_parser.cpp
@@ -0,0 +1,79 @@
+#include <variant>
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include <pattern_parser/parser.hpp>
+#include <pattern_parser/tokenizer.hpp>
+
+namespace
+{
+
+struct PatternParserTest : public ::testing::Test
+{
+    void SetUp(void) { std::cout << "Setup!\n"; }
+};
+
+TEST_F(PatternParserTest, TestEmptyPattern)
+{
+
+    std::string pattern{""};
+    auto tokenizer = std::make_shared<pattern_parser::Tokenizer>(pattern);
+    auto pattern_parzer = std::make_shared<pattern_parser::Parser>(tokenizer);
+    std::shared_ptr<pattern_parser::PatternNode> pattern_root =
+        pattern_parzer->ParsePattern();
+
+    EXPECT_EQ(0, pattern_root->NumEvents());
+}
+
+TEST_F(PatternParserTest, TestSingleEventPattern)
+{
+
+    std::string pattern{"bd"};
+    auto tokenizer = std::make_shared<pattern_parser::Tokenizer>(pattern);
+    auto pattern_parzer = std::make_shared<pattern_parser::Parser>(tokenizer);
+    std::shared_ptr<pattern_parser::PatternNode> pattern_root =
+        pattern_parzer->ParsePattern();
+
+    EXPECT_EQ(1, pattern_root->NumEvents());
+
+    std::shared_ptr<pattern_parser::EventGroup> events =
+        std::dynamic_pointer_cast<pattern_parser::EventGroup>(pattern_root);
+    if (!events)
+        FAIL() << "Cannot cast pattern_root to EventGroup!";
+}
+
+TEST_F(PatternParserTest, TestNestedEventPattern)
+{
+
+    std::string pattern{"bd*3 sd"};
+    auto tokenizer = std::make_shared<pattern_parser::Tokenizer>(pattern);
+    auto pattern_parzer = std::make_shared<pattern_parser::Parser>(tokenizer);
+    std::shared_ptr<pattern_parser::PatternNode> pattern_root =
+        pattern_parzer->ParsePattern();
+
+    EXPECT_EQ(2, pattern_root->NumEvents());
+
+    std::shared_ptr<pattern_parser::EventGroup> events =
+        std::dynamic_pointer_cast<pattern_parser::EventGroup>(pattern_root);
+    if (!events)
+        FAIL() << "Cannot cast pattern_root to EventGroup!";
+
+    auto nested_bd = std::dynamic_pointer_cast<pattern_parser::EventGroup>(
+        events->events_[0]);
+    if (!nested_bd)
+        FAIL() << "Cannot cast nested_bd to EventGroup!";
+    EXPECT_EQ(3, nested_bd->NumEvents());
+
+    // snare
+    EXPECT_EQ(1, events->events_[1]->NumEvents());
+}
+
+} // namespace
