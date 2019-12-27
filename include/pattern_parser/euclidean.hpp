415d884947cc67630c6d2146f27a979348051ff4
Thor Sideburns <thorsten.sideb0ard@gmail.com>
2019-12-26 18:33:19 -0800

    new euclidean rhythm implementation

---
+++ include/pattern_parser/euclidean.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+std::string generate_euclidean_string(int num_hits, int len_sequence);
+std::vector<std::vector<int>>
+euclidean_recursor(std::vector<std::vector<int>> front,
+                   std::vector<std::vector<int>> back);
