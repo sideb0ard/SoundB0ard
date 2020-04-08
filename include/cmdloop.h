a7682faba2d32884a3661540b92f9cdba4564e74
Thor Sideburns <thorsten.sideb0ard@gmail.com>
2020-04-07 20:00:54 -0700

    tidyn

--- include/cmdloop.h
+++ include/cmdloop.h
@@ -20,10 +20,3 @@ bool parse_dxsynth_settings_change(dxsynth *ms, char wurds[][SIZE_OF_WURD]);
 bool parse_digisynth_settings_change(digisynth *ms, char wurds[][SIZE_OF_WURD]);
 
 bool is_valid_file(char *filename);
-
-int generic_osc_handler(const char *path, const char *types, lo_arg **argv,
-                        int argc, void *data, void *user_data);
-int trigger_osc_handler(const char *path, const char *types, lo_arg **argv,
-                        int argc, void *data, void *user_data);
-int osc_note_on_handler(const char *path, const char *types, lo_arg **argv,
-                        int argc, void *data, void *user_data);
