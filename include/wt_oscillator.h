629293cba1eddbada58b168970fd1442e403c533
Thorsten Sideboard <thorsten.sideb0ard@gmail.com>
2021-11-19 16:11:26 -0800

    Updated wavetable oscillators with std::array

--- include/wt_oscillator.h
+++ include/wt_oscillator.h
@@ -1,42 +1,42 @@
 #pragma once
+#include <array>
+
 #include "oscillator.h"
 
-#define WT_LENGTH 1024
-#define NUM_TABLES 9
+constexpr unsigned kWavetableLength = 16384;
+constexpr unsigned kNumTables = 9;
 
-struct WTOscillator : public Oscillator
-{
-    WTOscillator();
-    ~WTOscillator() = default;
-    double m_read_idx;
-    double m_wt_inc;
+struct WTOscillator : public Oscillator {
+  WTOscillator();
+  ~WTOscillator() = default;
+  double m_read_idx;
+  double m_wt_inc;
 
-    double m_read_idx2;
-    double m_wt_inc2;
+  double m_read_idx2;
+  double m_wt_inc2;
 
-    double m_sine_table[WT_LENGTH];
-    double *m_saw_tables[NUM_TABLES]{nullptr};
-    double *m_tri_tables[NUM_TABLES]{nullptr};
+  std::array<double, kWavetableLength> m_sine_table{};
+  std::array<std::array<double, kWavetableLength>, kNumTables> m_saw_tables{};
+  std::array<std::array<double, kWavetableLength>, kNumTables> m_tri_tables{};
 
-    double *m_current_table{nullptr};
-    int m_current_table_idx;
+  std::array<double, kWavetableLength> &m_current_table{m_sine_table};
+  int m_current_table_idx;
 
-    // correction factor
-    double m_square_corr_factor[NUM_TABLES];
+  // correction factor
+  std::array<double, kNumTables> m_square_corr_factor{};
 
-    void StartOscillator() override;
-    void StopOscillator() override;
-    void Reset() override;
-    void Update() override;
-    double DoOscillate(double *quad_output) override;
+  void StartOscillator() override;
+  void StopOscillator() override;
+  void Reset() override;
+  void Update() override;
+  double DoOscillate(double *quad_output) override;
 
-    double DoWaveTable(double *read_idx, double wt_inc);
-    double DoSquareWave();
-    double DoSquareWaveCore(double *read_idx, double wt_inc);
+  double DoWaveTable(double *read_idx, double wt_inc);
+  double DoSquareWave();
+  double DoSquareWaveCore(double *read_idx, double wt_inc);
 
-    void SelectTable();
-    int GetTableIndex();
+  void SelectTable();
+  int GetTableIndex();
 
-    void CreateWaveTables();
-    void DestroyWaveTables();
+  constexpr void CreateWaveTables();
 };
--- src/wt_oscillator.cpp
+++ src/wt_oscillator.cpp
@@ -9,16 +9,13 @@
 #include "utils.h"
 
 WTOscillator::WTOscillator() {
-  memset(m_saw_tables, 0, NUM_TABLES * sizeof(double));
-  memset(m_tri_tables, 0, NUM_TABLES * sizeof(double));
-
   m_read_idx = 0;
   m_wt_inc = 0;
   m_read_idx2 = 0;
   m_wt_inc2 = 0;
   m_current_table_idx = 0;
 
-  m_current_table = &m_sine_table[0];
+  m_current_table = m_sine_table;
 
   m_square_corr_factor[0] = 0.5;
   m_square_corr_factor[1] = 0.5;
@@ -67,12 +64,13 @@ double WTOscillator::DoOscillate(double *quad_outval) {
 
 double WTOscillator::DoWaveTable(double *read_idx, double wt_inc) {
   double out = 0.;
-  double mod_read_idx = *read_idx + m_phase_mod * WT_LENGTH;
+  double mod_read_idx = *read_idx + m_phase_mod * kWavetableLength;
   CheckWrapIndex(&mod_read_idx);
 
   int i_read_idx = abs((int)mod_read_idx);
   float frac = mod_read_idx - i_read_idx;
-  int read_idx_next = i_read_idx + 1 > WT_LENGTH - 1 ? 0 : i_read_idx + 1;
+  int read_idx_next =
+      i_read_idx + 1 > kWavetableLength - 1 ? 0 : i_read_idx + 1;
   out = utils::LinTerp(0, 1, m_current_table[i_read_idx],
                        m_current_table[read_idx_next], frac);
   *read_idx += wt_inc;
@@ -87,14 +85,14 @@ double WTOscillator::DoSquareWave() {
 
 double WTOscillator::DoSquareWaveCore(double *read_idx, double wt_inc) {
   double pw = m_pulse_width / 100;
-  double pwidx = *read_idx + pw * WT_LENGTH;
+  double pwidx = *read_idx + pw * kWavetableLength;
 
   double saw1 = DoWaveTable(read_idx, wt_inc);
 
   if (wt_inc >= 0) {
-    if (pwidx >= WT_LENGTH) pwidx = pwidx - WT_LENGTH;
+    if (pwidx >= kWavetableLength) pwidx = pwidx - kWavetableLength;
   } else {
-    if (pwidx < 0) pwidx = pwidx + WT_LENGTH;
+    if (pwidx < 0) pwidx = pwidx + kWavetableLength;
   }
 
   double saw2 = DoWaveTable(&pwidx, wt_inc);
@@ -115,14 +113,14 @@ void WTOscillator::StopOscillator() { m_note_on = false; }
 
 void WTOscillator::Update() {
   Oscillator::Update();
-  m_wt_inc = (double)WT_LENGTH * m_inc;
+  m_wt_inc = (double)kWavetableLength * m_inc;
   SelectTable();
 }
 
 void WTOscillator::SelectTable() {
   m_current_table_idx = GetTableIndex();
   if (m_current_table_idx < 0) {
-    m_current_table = &m_sine_table[0];
+    m_current_table = m_sine_table;
     return;
   }
 
@@ -137,7 +135,7 @@ int WTOscillator::GetTableIndex() {
   if (m_waveform == SINE) return -1;
 
   double seed_freq = 27.5;  // A0
-  for (int i = 0; i < NUM_TABLES; ++i) {
+  for (int i = 0; i < kNumTables; ++i) {
     if (m_fo <= seed_freq) return i;
 
     seed_freq *= 2.0;
@@ -145,33 +143,36 @@ int WTOscillator::GetTableIndex() {
   return -1;
 }
 
-void WTOscillator::CreateWaveTables() {
-  for (int i = 0; i < WT_LENGTH; i++) {
-    m_sine_table[i] = sin((double)i / WT_LENGTH) * (TWO_PI);
-  }
+constexpr void WTOscillator::CreateWaveTables() {
+  auto fill_sin = [i = std::size_t(0)]() mutable {
+    return sin((double)i / kWavetableLength) * (TWO_PI);
+  };
+  std::generate(m_sine_table.begin(), m_sine_table.end(), fill_sin);
+
   double seed_freq = 27.5;  // A0
-  for (int i = 0; i < NUM_TABLES; ++i) {
-    double *saw_table = (double *)calloc(WT_LENGTH, sizeof(double));
-    double *tri_table = (double *)calloc(WT_LENGTH, sizeof(double));
+  for (int i = 0; i < kNumTables; ++i) {
+    std::array<double, kWavetableLength> &saw_table = m_saw_tables[i];
+    std::array<double, kWavetableLength> &tri_table = m_tri_tables[i];
+
     int harms = (int)((SAMPLE_RATE / 2.0 / seed_freq) - 1.0);
     int half_harms = (int)((float)harms / 2.0);
     double max_saw = 0;
     double max_tri = 0;
 
-    for (int j = 0; j < WT_LENGTH; ++j) {
+    for (int j = 0; j < kWavetableLength; ++j) {
       for (int g = 1; g <= harms; ++g) {
         double x = g * M_PI / harms;
         double sigma = sin(x) / x;
         if (g == 1) sigma = 1.0;
         double n = g;
         saw_table[j] += pow((float)-1.0, (float)(g + 1)) * (1.0 / n) * sigma *
-                        sin(TWO_PI * j * n / WT_LENGTH);
+                        sin(TWO_PI * j * n / kWavetableLength);
       }
       for (int g = 1; g <= half_harms; ++g) {
         double n = g;
         tri_table[j] += pow((float)-1.0, (float)n) *
                         (1.0 / pow((float)(2 * n + 1), (float)2.0)) *
-                        sin(TWO_PI * (2.0 * n + 1) * j / WT_LENGTH);
+                        sin(TWO_PI * (2.0 * n + 1) * j / kWavetableLength);
       }
 
       if (j == 0) {
@@ -182,36 +183,18 @@ void WTOscillator::CreateWaveTables() {
         if (tri_table[j] > max_tri) max_tri = tri_table[j];
       }
     }
+
     // normalize
-    for (int j = 0; j < WT_LENGTH; ++j) {
+    for (int j = 0; j < kWavetableLength; ++j) {
       saw_table[j] /= max_saw;
       tri_table[j] /= max_tri;
     }
-
-    // store
-    m_saw_tables[i] = saw_table;
-    m_tri_tables[i] = tri_table;
 
     seed_freq *= 2.0;
   }
 }
 
-void WTOscillator::DestroyWaveTables() {
-  for (int i = 0; i < NUM_TABLES; ++i) {
-    double *p = m_saw_tables[i];
-    if (p) {
-      free(p);
-      m_saw_tables[i] = NULL;
-    }
-    p = m_tri_tables[i];
-    if (p) {
-      free(p);
-      m_tri_tables[i] = NULL;
-    }
-  }
-}
-
 inline void wt_check_wrap_index(double *index) {
-  while (*index < 0.0) *index += WT_LENGTH;
-  while (*index >= WT_LENGTH) *index -= WT_LENGTH;
+  while (*index < 0.0) *index += kWavetableLength;
+  while (*index >= kWavetableLength) *index -= kWavetableLength;
 }
